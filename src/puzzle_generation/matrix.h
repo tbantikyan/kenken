7efb9e1bf95802536208591e9c1166f3c1616fdb
Tigran Bantikyan <59093401+tbantikyan@users.noreply.github.com>
2026-04-21 00:20:08 -0400

    feat: implemented matrix (#4)

---
+++ src/puzzle_generation/matrix.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <vector>
+
+#include "common/types.h"
+
+namespace kenken {
+
+class Matrix {
+public:
+  Matrix(uchar size);
+
+  uchar GetCell(uchar row, uchar col) const;
+
+  void SetCell(uchar row, uchar col, uchar value);
+
+  uchar Size() const;
+
+private:
+  uchar size_;
+
+  std::vector<std::vector<uchar>> matrix_;
+};
+
+} // namespace kenken
