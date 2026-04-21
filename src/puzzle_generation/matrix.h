d20fa2627e8281325c858c463a6f988d0a61686d
Tigran Bantikyan <tigranbantikyan@gmail.com>
2026-04-21 00:34:07 -0400

    style: formatted pre-rule files

--- src/puzzle_generation/matrix.h
+++ src/puzzle_generation/matrix.h
@@ -7,19 +7,19 @@
 namespace kenken {
 
 class Matrix {
-public:
-  Matrix(uchar size);
+  public:
+    Matrix(uchar size);
 
-  uchar GetCell(uchar row, uchar col) const;
+    uchar GetCell(uchar row, uchar col) const;
 
-  void SetCell(uchar row, uchar col, uchar value);
+    void SetCell(uchar row, uchar col, uchar value);
 
-  uchar Size() const;
+    uchar Size() const;
 
-private:
-  uchar size_;
+  private:
+    uchar size_;
 
-  std::vector<std::vector<uchar>> matrix_;
+    std::vector<std::vector<uchar>> matrix_;
 };
 
 } // namespace kenken
